c66483bc776f1fd546dcc0f9a564c7cb68cd10f6
sujal <pandeysujal258@gmail.com>
2025-06-01 16:56:25 +0545

    Add campaign section components: CampaignGrid, CampaignCard, WhyRiseEasy, StartCampaign, and Footer

--- src/components/Pages/Homepage.tsx
+++ src/components/Pages/Homepage.tsx
@@ -1,6 +1,10 @@
-import Header from "../ui/Header/Header";
-import Hero from "../sections/Hero/Hero";
-import SearchFilter from "../sections/SearchFilter/SearchFilter";
+import Header from "../common/Header/Header";
+import Hero from "../common/Hero/Hero";
+import SearchFilter from "../common/SearchFilter/SearchFilter";
+import CampaignGrid from "../common/CampaignGrid/CampaignGrid";
+import WhyRiseEasy from "../common/WhyRiseEasy/WhyRiseEasy";
+import StartCampaign from "../common/StartCampign/StartCampign";
+import Footer from "../common/Footer/Footer";
 
 function Homepage() {
   return (
@@ -8,6 +12,10 @@ function Homepage() {
       <Header />
       <Hero />
       <SearchFilter />
+      <CampaignGrid />
+      <WhyRiseEasy />
+      <StartCampaign />
+      <Footer />
     </>
   );
 }
